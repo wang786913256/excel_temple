5fb476a4d81ab76504c5424cf73b70eb029077ea
wang <786913256@qq.com>
2017-03-13 11:55:22 +0800

    test5

--- run_hook.py
+++ run_hook.py
@@ -1,4 +1,16 @@
 from wsgiref.simple_server import make_server
-from webhook import application
-httpd = make_server("localhost", 5500, application)
-httpd.serve_forever()
+import os
+
+def application(environ, start_response):
+ start_response('200 OK', [('Content-Type','text/html')])
+ #os.system('git add .')
+ #os.system('git commit -m "merge"')
+ #os.system('git pull origin master')
+ os.system('git pull')
+ print "success"
+ return ['My own hello webhook']
+
+if __name__ == "__main__":
+    httpd = make_server("localhost", 5500, application)
+    # httpd.serve_forever()
+    httpd.handle_request()
