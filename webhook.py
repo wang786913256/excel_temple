8f2b4bdf557809522ffac9a7cb14cc757e172e45
wang <786913256@qq.com>
2017-03-13 11:29:59 +0800

    test5

--- webhook.py
+++ webhook.py
@@ -5,5 +5,6 @@ def application(environ, start_response):
  #os.system('git commit -m "merge"')
  #os.system('git pull origin master')
  os.system('git pull')
+
  print "success"
  return ['My own hello webhook']
