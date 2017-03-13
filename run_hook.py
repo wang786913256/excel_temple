2f8bf8c95e1ffb8fdcae890bb999f4cf78de750e
wang <786913256@qq.com>
2017-03-13 15:12:11 +0800

    test5

--- run_hook.py
+++ run_hook.py
@@ -10,7 +10,7 @@ def application(environ, start_response):
     except (ValueError):
         request_body_size = 0
     request_body = environ['wsgi.input'].read(request_body_size)
-    if not request_body:
+    if request_body:
         print type(request_body)
         print [request_body]
         request_body = json.loads(request_body)
