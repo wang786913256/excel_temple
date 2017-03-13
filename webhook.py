526e7a4546e21e8e47eb3f34e805343f37d94f0d
wang <786913256@qq.com>
2017-03-13 11:29:25 +0800

    test5

--- webhook.py
+++ webhook.py
@@ -6,5 +6,4 @@ def application(environ, start_response):
  #os.system('git pull origin master')
  os.system('git pull')
  print "success"
-
- # return ['My own hello webhook']
+ return ['My own hello webhook']
